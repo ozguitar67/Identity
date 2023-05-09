d9f1eee51523fa8f41dff8ed3eedb10b262697ed
ozguitar67 <ozguitar67@gmail.com>
2023-05-09 16:42:29 -0400

    Start Identity with EF Core

---
+++ Web App/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Web_App.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // You can globally assign schema here
+            modelBuilder.HasDefaultSchema("user");
+        }
+    }
+}
